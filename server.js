863e36633a46fa3f02177159518dc66144049ac8
Elier H <elierporto@gmail.com>
2021-12-11 16:04:21 -0500

    passport config

--- server.js
+++ server.js
@@ -2,10 +2,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 mongoose
   .connect("mongodb://localhost:27017/auth")
   .catch((error) => handleError(error));
+
+// Config
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "temporary-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+// Passport Config
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/users", (req, res) => {
   const accounts = [
